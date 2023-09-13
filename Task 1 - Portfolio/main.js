690ae563be8d8745698104d9d7337ebbc3232e91
ChandiniR09 <107064299+ChandiniR09@users.noreply.github.com>
2023-09-13 15:24:02 +0530

    Add files via upload

---
+++ Task 1 - Portfolio/main.js
@@ -0,0 +1,21 @@
+function downloadResume() {
+    window.location.href = 'https://drive.google.com/file/d/1mkJABLgtaeoddtEbPJV32Yu87oAjafCQ/view?usp=sharing';
+}
+function downloadCertificate1() {
+    window.location.href = 'https://drive.google.com/file/d/1zr6pp0BISJoNM8gZBk8j4tp1uA61N50c/view?usp=drive_link';
+}
+function downloadCertificate2() {
+    window.location.href = 'https://drive.google.com/file/d/1znR35nAL7xSsgKS52L6MLef7FjgIuTIR/view?usp=drive_link';
+}
+function downloadCertificate3() {
+    window.location.href = 'https://drive.google.com/file/d/1zk2BVPCIfdMxeL89hOZSp3BrNOSBmKz8/view?usp=drive_link';
+}
+function downloadCertificate4() {
+    window.location.href = 'https://drive.google.com/file/d/1zjjiyX7WW-ZmLpyyZsJbYQ-FE5F_dCpw/view?usp=drive_link';
+}
+function downloadCertificate5() {
+    window.location.href = 'https://drive.google.com/file/d/1zgToKQ-T3mbuGGz7WHriLtmdiC4YC4j_/view?usp=drive_link';
+}
+function downloadCertificate6() {
+    window.location.href = 'https://drive.google.com/file/d/1zgGDu9A1jFdHbLrw4v0cjIaztwqQ3Lz_/view?usp=drive_link';
+}
